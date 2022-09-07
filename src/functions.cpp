81c929544f1a4d0974a2c381bb1d2c957358e887
Arman Randhawa <44068586+ArmanR9@users.noreply.github.com>
2022-09-06 23:03:45 -0700

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,18 +1,13 @@
 #include "functions.h"
 
 void print_message(const char* message){
-
-  for(int i = 0; i < 8; i++){
-
-   lcd::print(i, message);
-
+  for (int i = 0; i < 8; i++) {
+      lcd::print(i, message);
   }
 }
 
 void clear_scr(){
-  int x = 0;
-  while(x < 8){
-  lcd::clear_line(x);
-  x += 1;
+  for (int i = 0; i < 8; i++) {
+      lcd::clear_line(i);
   }
-  }
+}
